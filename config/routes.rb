1d08d584e82c88754abfb1de99556edc5da4a816
JanaVPetrova <janavpetrova@gmail.com>
2013-05-17 00:38:00 +0400

    twitter authorization

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Authproviders::Application.routes.draw do
   resources :posts
   resources :admin
   root :to => 'posts#index'
+  match "users/auth/twitter" => "users/omniauth_callbacks#twitter"
+  #match "/signout" => "sessions#destroy", :as => :signout
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514212140) do
+ActiveRecord::Schema.define(:version => 20130516175044) do
 
   create_table "posts", :force => true do |t|
     t.string   "pet_name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130514212140) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
